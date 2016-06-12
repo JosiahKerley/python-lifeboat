6a94db4238fed27a0f2d07c363a30f1b4f0e3102
Josiah Kerley <josiahkerley@gmail.com>
2016-06-12 17:32:38 -0600

    ci-trigger: Sun Jun 12 17:32:38 MDT 2016

--- lifeboat/data/__init__.py
+++ lifeboat/data/__init__.py
@@ -3,14 +3,15 @@ from lifeboat import *
 from jsonpath_rw import parse
 
 class ModelData:
+  model = {}
   def __init__(self):
     self.output = utils.Output()
     self.output.console('Initializing data model',depth=3)
   def query(self,query):
-
+    return ( parse(query).find(self.model) )
 
 class InMemory_Data(ModelData):
-  model = {}
+  pass
 
 
 ## Plumbing
