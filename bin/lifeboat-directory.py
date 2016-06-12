e8470d43d7d2da55573e0e622ae4310e38a82c29
Josiah Kerley <josiahkerley@gmail.com>
2016-06-12 14:38:42 -0600

    ci-trigger: Sun Jun 12 14:38:42 MDT 2016

--- bin/lifeboat-directory.py
+++ bin/lifeboat-directory.py
@@ -10,7 +10,6 @@ results = parser.parse_args()
 print results.configfile
 
 lifeboat.services.Director.configfile = lifeboat.utils.File(results.configfile)
-
 print lifeboat.services.Director.configfile
 director = lifeboat.services.Director()
 director.start()
