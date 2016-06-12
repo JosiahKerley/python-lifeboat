20f9012f4450aff2352e118d2ee6c53df12bdf5b
Josiah Kerley <josiahkerley@gmail.com>
2016-06-12 17:31:17 -0600

    ci-trigger: Sun Jun 12 17:31:17 MDT 2016

--- lifeboat/data/__init__.py
+++ lifeboat/data/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 from lifeboat import *
-
+from jsonpath_rw import parse
 
 class ModelData:
   def __init__(self):
     self.output = utils.Output()
     self.output.console('Initializing data model',depth=3)
   def query(self,query):
-    
+
 
 class InMemory_Data(ModelData):
   model = {}
--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
     'jinja2',
     'requests',
     'zmq',
+    'jsonpath_rw',
   ]
 )
 
