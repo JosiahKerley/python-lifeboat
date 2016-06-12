454d86392653128f6655c8b22e263c92bda365bb
Josiah Kerley <josiahkerley@gmail.com>
2016-06-12 14:37:06 -0600

    ci-trigger: Sun Jun 12 14:37:06 MDT 2016

--- bin/lifeboat-directory.py
+++ bin/lifeboat-directory.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description='Lifeboat Director Daemon')
 parser.add_argument('--config', '-c', action="store", dest="configfile", default=False, required=True, help='Configuration file path')
 parser.add_argument('--version', action='version', version='%(prog)s 1.0.0')
 results = parser.parse_args()
+print results.configfile
 
 lifeboat.services.Director.configfile = results.configfile
 director = lifeboat.services.Director()
