4c37daacb321d0ecbf3a00070b9ce24e65cbb6ff
Josiah Kerley <josiahkerley@gmail.com>
2016-06-12 14:59:33 -0600

    ci-trigger: Sun Jun 12 14:59:33 MDT 2016

--- lifeboat/__init__.py
+++ lifeboat/__init__.py
@@ -2,12 +2,15 @@
 
 ## Modules
 class Module:
-  name         = None
-  description  = None
-  version      = None
-  author_name  = None
-  author_email = None
-  base_type    = 'module'
+  from lifeboat import utils
+  output        = utils.Output()
+  output.prefix = '\t\tModule: '
+  name          = None
+  description   = None
+  version       = None
+  author_name   = None
+  author_email  = None
+  base_type     = 'module'
   def test(self):
     print '{} works!'.format(self.name)
     return True
