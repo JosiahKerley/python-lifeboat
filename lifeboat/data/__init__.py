ba184099d11f028b92ecfd5d8bcd9b59d955b762
Josiah Kerley <josiahkerley@gmail.com>
2016-06-12 17:25:05 -0600

    ci-trigger: Sun Jun 12 17:25:05 MDT 2016

--- lifeboat/data/__init__.py
+++ lifeboat/data/__init__.py
@@ -2,10 +2,15 @@
 from lifeboat import *
 
 
-class InMemory_Data:
+class ModelData:
+  def __init__(self):
+    self.output = utils.Output()
+    self.output.console('Initializing data model',depth=1)
+
+class InMemory_Data(ModelData):
   model = {}
 
 
-
+## Plumbing
 class Data(InMemory_Data):
   pass
