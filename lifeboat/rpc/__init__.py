8d7095ea1c97b001814b5d7548954b4d436a3218
Josiah Kerley <josiahkerley@gmail.com>
2016-06-12 15:14:21 -0600

    ci-trigger: Sun Jun 12 15:14:21 MDT 2016

--- lifeboat/rpc/__init__.py
+++ lifeboat/rpc/__init__.py
@@ -15,3 +15,9 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:%s" % port)
 
+while True:
+    #  Wait for next request from client
+    message = socket.recv()
+    print "Received request: ", message
+    time.sleep (1)
+    socket.send("World from %s" % port)
