fb4f00383cb2a9ae9237b4ba5544e2cc0ae08787
Josiah Kerley <josiahkerley@gmail.com>
2016-06-12 18:21:52 -0600

    ci-trigger: Sun Jun 12 18:21:52 MDT 2016

--- lifeboat/data/__init__.py
+++ lifeboat/data/__init__.py
@@ -9,8 +9,10 @@ class ModelData:
     self.output = utils.Output()
     self.output.console('Initializing data model',depth=3)
   def query(self,query):
-    return (parse(query).find(self.model))
-
+    try:
+      return(parse(query).find(self.model))
+    except:
+      return({'error':'Query "{}" failed'.format(query)})
 
 class InMemory_Data(ModelData):
   pass
