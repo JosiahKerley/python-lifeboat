a3e3bb74b49d7c498308c7847380bdf4e249e441
Josiah Kerley <josiahkerley@gmail.com>
2016-06-12 18:16:38 -0600

    ci-trigger: Sun Jun 12 18:16:38 MDT 2016

--- bin/lifeboat-console.py
+++ bin/lifeboat-console.py
@@ -7,5 +7,7 @@ import lifeboat
 client = lifeboat.clients.Client('tcp://127.0.0.1:7111')
 print client
 print dir(client)
-client.connection.send({'get':'$.'})
+message = {'get':'$.'}
+print 'sending '+str(message)
+client.connection.send(message)
 
